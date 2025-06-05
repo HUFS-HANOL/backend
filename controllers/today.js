6b25278ad4cc194dadb7a626697c80fbf9439834
mapagd <jppam1128@gmail.com>
2025-06-06 05:13:25 +0900

    today v1.3

--- controllers/today.js
+++ controllers/today.js
@@ -4,7 +4,7 @@ const db = require("../models/database");
 exports.todayDiaryDb = async(req, res) => {
     const {user_id, content, emotion_type} = req.body;
     try {
-        const sql = "INSERT INTO diaries (user_id, content, emotion_type, created_at) VALUES (?, ?,  NOW())";
+        const sql = "INSERT INTO diaries (user_id, content, emotion_type, created_at) VALUES (?, ?, ?,NOW())";
         db.query(sql, [user_id, content, emotion_type], (err, result) => {
             if (err) {
                 console.error("데이터 베이스 오류:", err);
@@ -23,7 +23,7 @@ exports.todayDiaryDb = async(req, res) => {
 
 // 시 값 프론트 전송
 exports.todayPoem = async(req,res) => {
-    const {content} = req.body;
+    const {content,diary_id,emotion_type} = req.body;
     if (!content) {
         return res.status(400).json({ diaryMessage: "일기 전송에 실패했습니다." });
     }
@@ -31,10 +31,12 @@ exports.todayPoem = async(req,res) => {
         //시, 문구 생성 반환 함수 실행 후 값 할당
         const poemText = "봄비는 \n 간질이는 손가락을 갖고 있나? \n 대지가 풋사랑에 빠진 것 같다 "
         const phraseText = "설레는 당신의 순간을 함께 응원할게요."
+        const emotion_id = await todayEmotion(diary_id,emotion_type);
     res.json(
         {
             poem: poemText,
             phrase: phraseText,
+            emotion_id: emotion_id
 
         }
     )
@@ -52,44 +54,43 @@ exports.todayPoem = async(req,res) => {
 // (감정 생성) = 감정 이미지 반환 함수
 // 매개변수: 감정 값 , 리턴 값: 감정 이미지 url , emotion_id = emotion_id
 
-// // 감정 생성 함수
-// async function todayEmotion(diary_id) {
-//     const sql = "INSERT INTO emotions (diary_id, emotion_type, created_at) VALUES (?,?,?,NOW())";
-//     emotionType = "행복함"
-//     try {
-//         const emotion_id = await new Promise((resolve, reject) => {
-//             db.query(sql, [diary_id, emotionType, emotionScore], (err, result) => {
-//                 if (err) {
-//                     console.error("데이터베이스 오류:", err);
-//                     return reject(err); 
-//                 }
-//                 resolve(result.insertId); 
-//             });
-//         });
+// 감정 생성 함수
+async function todayEmotion(diary_id,emotion_type) {
+    const sql = "INSERT INTO emotions (diary_id, emotion_type, created_at) VALUES (?,?,NOW())";
+    try {
+        const emotion_id = await new Promise((resolve, reject) => {
+            db.query(sql, [diary_id, emotion_type], (err, result) => {
+                if (err) {
+                    console.error("데이터베이스 오류:", err);
+                    return reject(err); 
+                }
+                resolve(result.insertId); 
+            });
+        });
 
-//         console.log("감정 저장 완료, emotion_id:", emotion_id);
-//         return emotion_id; 
-//     } catch (err) {
-//         console.error("생성된 감정 저장 중 오류 발생:", err);
-//         throw err; 
-//     }
-// }
+        console.log("감정 저장 완료, emotion_id:", emotion_id);
+        return emotion_id; 
+    } catch (err) {
+        console.error("생성된 감정 저장 중 오류 발생:", err);
+        throw err; 
+    }
+}
 
 // 시 데이터베이스 전송
 exports.todayPoemDb = async(req, res) => {
-    const { diary_id, poem} = req.body;
+    const { diary_id, poem, emotion_id} = req.body;
     if(!diary_id){
         return res.status(400).json({diaryMessage:"일기를 저장해주세요."});
     }
     try {
-        const sql = "INSERT INTO poems (diary_id, poem_text, created_at) VALUES (?, ?, ?, NOW())";
-        db.query(sql, [diary_id, poem], (err, result) => {
+        const sql = "INSERT INTO poems (diary_id, emotion_id, poem_text, created_at) VALUES (?, ?, ?, NOW())";
+        db.query(sql, [diary_id, emotion_id, poem], (err, result) => {
             if (err) {
                 console.error("데이터 베이스 오류:", err);
                 return res.status(400).json({ diaryMessage: "서버가 요청을 처리할 수 없습니다. 나중에 다시 시도해주세요." });
             }
             console.log("시 데이터베이스 저장 완료");
-            res.status(201).json({diaryMessage:"시 저장 완료."});
+            res.status(201).json({diaryMessage:"시 저장 완료.",poem_id: result.insertId});
         });
     } catch (err) {
         console.error(err);
