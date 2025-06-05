2f6dfb52a31969aecab794ee33d04373f968e5ba
mapagd <jppam1128@gmail.com>
2025-06-06 04:41:49 +0900

    today v1.3

--- routes/todayRoutes.js
+++ routes/todayRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/diaries", today.todayDiaryDb);
 router.post("/poemphrase", today.todayPoem);
 router.post("/poems",today.todayPoemDb);
+router.post("/likepoem",today.todayLikePoem);
 
 module.exports = router;
