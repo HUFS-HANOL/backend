43c086361e9dc9bc3513e74c411e907362c041b4
mapagd <jppam1128@gmail.com>
2025-06-08 23:39:42 +0900

    통합 실수 복구구

--- routes/likedPoemsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const likedPoems = require("../controllers/likedPoems");
-
-const router = express.Router();
-
-router.get("/view",likedPoems.likedPoemsView);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,8 @@
+// server.js
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`✅ 서버 실행 중: http://localhost:${PORT}`);
-
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
-const todayRoutes = require("./routes/todayRoutes");
-const likedPoemsRoutes = require("./routes/likedPoemsRoutes")
-
-const app = express();
-const PORT = 3000;
-
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/today",todayRoutes);
-app.use("/api/likedPoems",likedPoemsRoutes);
-
+});
