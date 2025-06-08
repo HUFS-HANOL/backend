7171f5970b9473e26bc344d44883cf43e548a755
mapagd <jppam1128@gmail.com>
2025-06-09 02:53:23 +0900

    업로드

---
+++ routes/authRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const auth = require("../controllers/auth");
+
+const devLogin = require("../controllers/devLogin")
+
+
+const router = express.Router();
+
+router.post("/register", auth.register);
+
+// router.post("/login", auth.login);
+router.post("/login", auth.login);
+router.post("/token", auth.refreshToken);
+router.post("/logout", auth.logout);
+router.post("/auth", auth.authenticateToken, (req, res) => {
+    res.status(200).json({ message: "유효한 토큰", user: req.user });
+});
+
+module.exports = router;
