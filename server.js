2e404d476facead6b86e77b60f97e9d8f0541fd8
202103382_진주연 <101190159+Est0Z@users.noreply.github.com>
2025-06-10 17:21:03 +0900

    날씨 API 추가

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const todayRoutes = require("./routes/todayRoutes");
 const likedPoemsRoutes = require("./routes/likedPoemsRoutes");
+const weatherRouter = require('./routes/weather'); // 주연
 const calendarRoutes = require('./routes/calendar');
 const statisticsRoutes = require('./routes/statistics');
 
@@ -24,6 +25,7 @@ app.use("/api/today",todayRoutes);
 app.use("/api/likedPoems",likedPoemsRoutes);
 app.use("/api/calendar",calendarRoutes);
 app.use("/api/statistics",statisticsRoutes);
+app.use('/api/weather', weatherRouter) // 주연
 
 // 서버 연결
 app.listen(PORT, () => {
